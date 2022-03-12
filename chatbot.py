917e6bb321c162fe973018ca40e04c945b138c3d
WaimenMak <1327149492@qq.com>
2022-03-12 11:32:22 +0800

    update

--- chatbot.py
+++ chatbot.py
@@ -13,12 +13,12 @@ def main():
     
     # config = configparser.ConfigParser()
     # config.read('config.ini')
-    # updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
+    updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
 
 
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    # config = configparser.ConfigParser()
+    # config.read('config.ini')
+    # updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
 
     global redis1
